2d0e2d051290cc7aa76b5c07bf7df29cb88de854
jgbor <100523032+jgbor@users.noreply.github.com>
2022-11-28 21:34:37 +0100

    items in list and pictures can de loaded

--- src/main/kotlin/ui/DealsWindow.kt
+++ src/main/kotlin/ui/DealsWindow.kt
@@ -1,18 +1,30 @@
 package ui
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.BitmapPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import model.DealData
 import network.NetworkManager
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.IOException
+import java.net.URL
 
 class DealsWindow {
     private var platform : String? = null
@@ -26,22 +38,25 @@ class DealsWindow {
     fun show() {
         loadDealData()
         MaterialTheme {
-            DealItem()
-            DealItem()
+            LazyColumn {
+                items(dealsList) { deal ->
+                    DealItem(deal)
+                }
+            }
         }
     }
 
     @Composable
     @Preview
-    fun DealItem(){
-        Scaffold {
-            Text(if (dealsList.size>0) "${dealsList[0].description}!" else "nincs adat")
-            var sz = remember{"Button"}
-            Button(onClick = {
-                sz = "Pressed!"
-            }){
-                Text(sz)
-            }
+    private fun DealItem(dealData: DealData){
+        Row{
+            AsyncImage(
+                load = { loadImageBitmap(dealData.thumbnail) },
+                painterFor = { remember { BitmapPainter(it) } },
+                contentDescription = "Sample",
+                modifier = Modifier.width(200.dp)
+            )
+            Text(dealData.description)
         }
     }
 
@@ -75,4 +90,38 @@ class DealsWindow {
             dealsList.add(dealData!!)
         }
     }
+
+    @Composable
+    private fun <T> AsyncImage(
+        load: suspend () -> T,
+        painterFor: @Composable (T) -> Painter,
+        contentDescription: String,
+        modifier: Modifier = Modifier,
+        contentScale: ContentScale = ContentScale.Fit,
+    ) {
+        val image: T? by produceState<T?>(null) {
+            value = withContext(Dispatchers.IO) {
+                try {
+                    load()
+                } catch (e: IOException) {
+                    // instead of printing to console, you can also write this to log,
+                    // or show some error placeholder
+                    e.printStackTrace()
+                    null
+                }
+            }
+        }
+
+        if (image != null) {
+            Image(
+                painter = painterFor(image!!),
+                contentDescription = contentDescription,
+                contentScale = contentScale,
+                modifier = modifier
+            )
+        }
+    }
+
+    private fun loadImageBitmap(url: String): ImageBitmap =
+        URL(url).openStream().buffered().use(::loadImageBitmap)
 }
